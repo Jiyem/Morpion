dc99146b46d1dbb7bed1a8167ea5f42872a82f64
domestit <domestit@pc-dg-037-04.iut2.upmf-grenoble.fr>
2018-12-05 10:44:42 +0100

    C'est les bons la :D

---
+++ Morpion/src/morpion/EtatCase.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package morpion;
+
+/**
+ *
+ * @author domestit
+ */
+public enum EtatCase {
+    X,
+    O,
+    NON_COCHEE;
+    
+}
