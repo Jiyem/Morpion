68091bb1d06cdf895b0f231e9e562e93791aa61f
domestit <domestit@pc-dg-037-04.iut2.upmf-grenoble.fr>
2018-12-08 11:58:35 +0100

    VueClassement0.5

--- Morpion/src/Vue/VueClassement.java
+++ Morpion/src/Vue/VueClassement.java
@@ -7,12 +7,15 @@ package Vue;
 
 import utilitaire.Actions;
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.util.Observable;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
@@ -31,21 +34,30 @@ public class VueClassement extends Observable{
         this.window = new JFrame();
         window.setSize(350, 200);
         //le titre = nom du joueur 
+        window.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+        // Définit la taille de la fenêtre en pixels
+        window.setSize(800, 300);
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        window.setLocation(dim.width/2-window.getSize().width/2, dim.height/2-window.getSize().height/2);
         window.setTitle("Classement : Base");
         mainPanel = new JPanel(new BorderLayout());
         this.window.add(mainPanel);
         
-        mainPanel.add(classementgeneral = new JButton("Classement general du tournoi"));
+        contentPanel = new JPanel (new GridLayout(3,3));
+        mainPanel.add(contentPanel, BorderLayout.CENTER);
+        contentPanel.add(new JLabel(""));contentPanel.add(new JLabel(""));contentPanel.add(new JLabel(""));
+        
+        
+        contentPanel.add(classementgeneral = new JButton("Classement general du tournoi"));
         classementgeneral.addActionListener((ActionEvent e) -> {
             setChanged();
             notifyObservers(Actions.CLASSEMENT_GENERAL);
             clearChanged();
         });
-        
-        contentPanel = new JPanel (new GridLayout(1,2));
-        mainPanel.add(contentPanel, BorderLayout.CENTER);
-        //contentPanel.
+        contentPanel.add(new JLabel(""));
         contentPanel.add(listeJoueur = new JComboBox());
+        
+        contentPanel.add(new JLabel(""));contentPanel.add(new JLabel(""));contentPanel.add(new JLabel(""));
         
     }
     
